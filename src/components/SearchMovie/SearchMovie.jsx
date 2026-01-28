6fd34f31f8a5362a3942fb381f36aeec677a4542
denis-mahei <denmahei@gmail.com>
2026-01-28 14:29:22 +0200

    fix searchMovies

--- src/components/SearchMovie/SearchMovie.jsx
+++ src/components/SearchMovie/SearchMovie.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import css from './SearchMovie.module.css';
 import { toast } from 'react-hot-toast';
 
-const SearchMovie = ({ onSearch, genre, localGenre, setLocalGenre }) => {
+const SearchMovie = ( { onSearch, localGenre } ) => {
   const [query, setQuery] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = ( e ) => {
     e.preventDefault();
 
     if (query === '') {
@@ -24,22 +24,10 @@ const SearchMovie = ({ onSearch, genre, localGenre, setLocalGenre }) => {
         type="text"
         name="search"
         value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={( e ) => setQuery(e.target.value)}
         autoComplete="off"
         className={css.field}
       />
-      <select
-        name="genres"
-        value={localGenre}
-        onChange={(e) => setLocalGenre(e.target.value)}
-      >
-        <option value="all">All</option>
-        {genre.map((item) => (
-          <option key={item.id} value={item.id}>
-            {item.name}
-          </option>
-        ))}
-      </select>
       <button type="submit" className={css.searchBtn}>
         Search
       </button>
